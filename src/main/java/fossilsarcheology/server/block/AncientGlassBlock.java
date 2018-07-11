99d110dc5e79607dc3ee742c00958b729b79e1b2
Alexthe666 <alex.rowlands@cox.net>
2018-07-11 14:25:03 -0700

    some connected texture porgress

--- src/main/java/fossilsarcheology/server/block/AncientGlassBlock.java
+++ src/main/java/fossilsarcheology/server/block/AncientGlassBlock.java
@@ -2,11 +2,15 @@ package fossilsarcheology.server.block;
 
 import fossilsarcheology.server.api.DefaultRenderedItem;
 import fossilsarcheology.server.tab.FATabRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockBreakable;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
@@ -16,60 +20,74 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class AncientGlassBlock extends BlockBreakable implements DefaultRenderedItem {
 
-	public static final PropertyBool CONNECTED_DOWN = PropertyBool.create("connected_down");
-	public static final PropertyBool CONNECTED_UP = PropertyBool.create("connected_up");
-	public static final PropertyBool CONNECTED_NORTH = PropertyBool.create("connected_north");
-	public static final PropertyBool CONNECTED_SOUTH = PropertyBool.create("connected_south");
-	public static final PropertyBool CONNECTED_WEST = PropertyBool.create("connected_west");
-	public static final PropertyBool CONNECTED_EAST = PropertyBool.create("connected_east");
+    public static final PropertyBool UP = PropertyBool.create("up");
+    public static final PropertyBool DOWN = PropertyBool.create("down");
+    public static final PropertyBool NORTH = PropertyBool.create("north");
+    public static final PropertyBool EAST = PropertyBool.create("east");
+    public static final PropertyBool SOUTH = PropertyBool.create("south");
+    public static final PropertyBool WEST = PropertyBool.create("west");
 
-	public AncientGlassBlock() {
-		super(Material.GLASS, true);
-		this.setLightOpacity(1);
-		this.setUnlocalizedName("ancient_glass");
-		this.setSoundType(SoundType.GLASS);
-		this.setHardness(1F);
-		this.setCreativeTab(FATabRegistry.BLOCKS);
-		//this.setDefaultState(this.blockState.getBaseState().withProperty(CONNECTED_DOWN, Boolean.FALSE).withProperty(CONNECTED_EAST, Boolean.FALSE).withProperty(CONNECTED_NORTH, Boolean.FALSE).withProperty(CONNECTED_SOUTH, Boolean.FALSE).withProperty(CONNECTED_UP, Boolean.FALSE).withProperty(CONNECTED_WEST, Boolean.FALSE));
-	}
-
-	@Override
-    @SideOnly(Side.CLIENT)
-	public boolean shouldSideBeRendered(IBlockState state, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-		IBlockState neighbour = blockAccess.getBlockState(pos.offset(side));
-		return state.getBlock() != neighbour.getBlock();
-	}
-
-   /* @Override
-    protected BlockStateContainer createBlockState () {
-        return new BlockStateContainer(this, new IProperty[] { CONNECTED_DOWN, CONNECTED_UP, CONNECTED_NORTH, CONNECTED_SOUTH, CONNECTED_WEST, CONNECTED_EAST });
+    public AncientGlassBlock() {
+        super(Material.GLASS, true);
+        this.setLightOpacity(1);
+        this.setUnlocalizedName("ancient_glass");
+        this.setSoundType(SoundType.GLASS);
+        this.setHardness(1F);
+        this.setCreativeTab(FATabRegistry.BLOCKS);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(UP, Boolean.valueOf(false))
+                .withProperty(DOWN, Boolean.valueOf(false))
+                .withProperty(NORTH, Boolean.valueOf(false))
+                .withProperty(EAST, Boolean.valueOf(false))
+                .withProperty(SOUTH, Boolean.valueOf(false))
+                .withProperty(WEST, Boolean.valueOf(false))
+        );
     }
 
-    @Override
-    public int getMetaFromState (IBlockState state) {
+    public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+        return state.withProperty(UP, canFenceConnectTo(worldIn, pos, EnumFacing.UP))
+                .withProperty(DOWN, canFenceConnectTo(worldIn, pos, EnumFacing.DOWN))
+                .withProperty(NORTH, canFenceConnectTo(worldIn, pos, EnumFacing.NORTH))
+                .withProperty(SOUTH, canFenceConnectTo(worldIn, pos, EnumFacing.SOUTH))
+                .withProperty(EAST, canFenceConnectTo(worldIn, pos, EnumFacing.EAST))
+                .withProperty(WEST, canFenceConnectTo(worldIn, pos, EnumFacing.WEST));
+    }
+
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[]{UP, DOWN, NORTH, SOUTH, EAST, WEST});
+    }
+
+    public int getMetaFromState(IBlockState state) {
         return 0;
     }
 
+    private boolean canFenceConnectTo(IBlockAccess world, BlockPos pos, EnumFacing facing) {
+        BlockPos other = pos.offset(facing);
+        Block block = world.getBlockState(other).getBlock();
+        return block == this;
+    }
+
     @Override
-    public IBlockState getActualState (IBlockState state, IBlockAccess world, BlockPos position) {
-        return state.withProperty(CONNECTED_DOWN, this.isSideConnectable(world, position, EnumFacing.DOWN)).withProperty(CONNECTED_EAST, this.isSideConnectable(world, position, EnumFacing.EAST)).withProperty(CONNECTED_NORTH, this.isSideConnectable(world, position, EnumFacing.NORTH)).withProperty(CONNECTED_SOUTH, this.isSideConnectable(world, position, EnumFacing.SOUTH)).withProperty(CONNECTED_UP, this.isSideConnectable(world, position, EnumFacing.UP)).withProperty(CONNECTED_WEST, this.isSideConnectable(world, position, EnumFacing.WEST));
-    }
-
-    private boolean isSideConnectable (IBlockAccess world, BlockPos pos, EnumFacing side) {
-        final IBlockState state = world.getBlockState(pos.offset(side));
-        return (state == null) ? false : state.getBlock() == this;
-    }
-    */
-
-	@Override
     @SideOnly(Side.CLIENT)
-	public BlockRenderLayer getBlockLayer() {
-		return BlockRenderLayer.TRANSLUCENT;
-	}
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.TRANSLUCENT;
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
+    @SuppressWarnings("deprecation")
+    @Override
     public boolean isFullCube(IBlockState state) {
-		return false;
-	}
+        return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+        IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
+        Block block = iblockstate.getBlock();
+        if (block == this) {
+            return false;
+        }
+        if (blockState != iblockstate) {
+            return true;
+        }
+        return super.shouldSideBeRendered(blockState, blockAccess, pos, side);
+    }
 }
