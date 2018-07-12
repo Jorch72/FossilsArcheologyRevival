40934eb637743ffe6d433920ab9a3c1dc42f21c1
Alexthe666 <alex.rowlands@cox.net>
2018-07-12 15:03:58 -0700

    reinforced glass done

--- src/main/java/fossilsarcheology/server/block/StrongGlassBlock.java
+++ src/main/java/fossilsarcheology/server/block/StrongGlassBlock.java
@@ -5,6 +5,9 @@ import fossilsarcheology.server.tab.FATabRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBreakable;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
@@ -15,6 +18,13 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class StrongGlassBlock extends BlockBreakable implements DefaultRenderedItem {
 
+	public static final PropertyBool UP = PropertyBool.create("up");
+	public static final PropertyBool DOWN = PropertyBool.create("down");
+	public static final PropertyBool NORTH = PropertyBool.create("north");
+	public static final PropertyBool EAST = PropertyBool.create("east");
+	public static final PropertyBool SOUTH = PropertyBool.create("south");
+	public static final PropertyBool WEST = PropertyBool.create("west");
+
 	public StrongGlassBlock() {
 		super(Material.GLASS, true);
 		this.setLightOpacity(1);
@@ -22,26 +32,61 @@ public class StrongGlassBlock extends BlockBreakable implements DefaultRenderedI
 		this.setHardness(3F);
 		this.setResistance(25F);
 		this.setCreativeTab(FATabRegistry.BLOCKS);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(UP, Boolean.valueOf(false))
+				.withProperty(DOWN, Boolean.valueOf(false))
+				.withProperty(NORTH, Boolean.valueOf(false))
+				.withProperty(EAST, Boolean.valueOf(false))
+				.withProperty(SOUTH, Boolean.valueOf(false))
+				.withProperty(WEST, Boolean.valueOf(false))
+		);
+	}
+
+	public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+		return state.withProperty(UP, canFenceConnectTo(worldIn, pos, EnumFacing.UP))
+				.withProperty(DOWN, canFenceConnectTo(worldIn, pos, EnumFacing.DOWN))
+				.withProperty(NORTH, canFenceConnectTo(worldIn, pos, EnumFacing.NORTH))
+				.withProperty(SOUTH, canFenceConnectTo(worldIn, pos, EnumFacing.SOUTH))
+				.withProperty(EAST, canFenceConnectTo(worldIn, pos, EnumFacing.EAST))
+				.withProperty(WEST, canFenceConnectTo(worldIn, pos, EnumFacing.WEST));
+	}
+
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[]{UP, DOWN, NORTH, SOUTH, EAST, WEST});
+	}
+
+	public int getMetaFromState(IBlockState state) {
+		return 0;
+	}
+
+	private boolean canFenceConnectTo(IBlockAccess world, BlockPos pos, EnumFacing facing) {
+		BlockPos other = pos.offset(facing);
+		Block block = world.getBlockState(other).getBlock();
+		return block == this;
 	}
 
 	@Override
-    @SideOnly(Side.CLIENT)
-	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-		IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
-		Block block = iblockstate.getBlock();
-        return blockState != iblockstate;
-	}
-
-	@Override
-    @SideOnly(Side.CLIENT)
+	@SideOnly(Side.CLIENT)
 	public BlockRenderLayer getBlockLayer() {
-		return BlockRenderLayer.CUTOUT;
+		return BlockRenderLayer.TRANSLUCENT;
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
-    public boolean isFullCube(IBlockState state) {
+	public boolean isFullCube(IBlockState state) {
 		return false;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+		IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
+		Block block = iblockstate.getBlock();
+		if (block == this) {
+			return false;
+		}
+		if (blockState != iblockstate) {
+			return true;
+		}
+		return super.shouldSideBeRendered(blockState, blockAccess, pos, side);
 	}
 
 }
