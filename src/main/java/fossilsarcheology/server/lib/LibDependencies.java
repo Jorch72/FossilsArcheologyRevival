a234f292acfc1f9a654a0eb1615f1d4f5639d77f
Sunconure11 <eyespot7b@aol.com>
2018-07-31 19:11:58 -0400

    Change load order mechanism, so this mod loads last
    At least, it should load last now.

---
+++ src/main/java/fossilsarcheology/server/lib/LibDependencies.java
@@ -0,0 +1,9 @@
+package fossilsarcheology.server.lib;
+
+/**
+ * Created by Joseph on 7/31/2018.
+ */
+public final class LibDependencies {
+
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.3.2655,];after:JEI@[4.10.0.198,];required-after:llibrary@[1.7.9,];after:ZZZ@[0.1]";
+}
