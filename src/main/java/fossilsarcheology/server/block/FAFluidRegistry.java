82e91fedadf2d7398a9fa5b45d0b26f20d440208
Alex-the-666 <alex.rowlands@cox.net>
2018-08-25 14:09:37 -0700

    fix #857

--- src/main/java/fossilsarcheology/server/block/FAFluidRegistry.java
+++ src/main/java/fossilsarcheology/server/block/FAFluidRegistry.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fluids.FluidRegistry;
 
 public class FAFluidRegistry {
 
-	public static final Fluid TAR_FLUID = new Fluid("tar", new ResourceLocation("fossil", "blocks/tar"), new ResourceLocation("fossil", "blocks/tar_flowing"));
+	public static final Fluid TAR_FLUID = new Fluid("tar", new ResourceLocation("fossil", "blocks/tar"), new ResourceLocation("fossil", "blocks/tar_flowing")).setDensity(3000).setTemperature(400).setViscosity(3000);
 	public static final Material TAR_MATERIAL = new MaterialTar();
 
 	public static void register() {
